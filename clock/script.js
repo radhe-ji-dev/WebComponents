3a32802ae10294ecf3f1d41a997b481e8c0a5b11
Radhakant Tiwari <therktiwadi@gmail.com>
2024-06-18 07:05:54 +0530

    clock added

---
+++ clock/script.js
@@ -0,0 +1,14 @@
+function updateClock() {
+  var now = new Date();
+  console.log(now);
+  var hours = now.getHours().toString().padStart(2, "0");
+  var minutes = now.getMinutes().toString().padStart(2, "0");
+  var seconds = now.getSeconds().toString().padStart(2, "0");
+
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
